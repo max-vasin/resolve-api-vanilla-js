d7d6337da033c097aec1386e9c4da3207518b65b
max <mozg.mayhem@gmail.com>
2020-02-11 14:26:14 +0300

    Upd

---
+++ client/index-new.js
@@ -0,0 +1,27 @@
+import domready from 'domready'
+import { getApi } from 'resolve-api'
+
+const main = async resolveContext => {
+  await new Promise(resolve => domready(resolve))
+  const api = getApi(resolveContext)
+
+  const sendMessage = (userName, message) =>
+    api.command(
+      {
+        aggregateName: 'Chat',
+        type: 'postMessage',
+        aggregateId: userName,
+        payload: message
+      },
+      err => {
+        if (err) {
+          console.warn(`Error while sending command: ${err}`)
+        }
+      }
+    )
+
+  initUI(sendMessage)
+
+}
+
+export default main
