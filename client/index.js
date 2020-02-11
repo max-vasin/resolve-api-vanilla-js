15c3ca0c19da135631b7453b17b92a2c62e4d7d5
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2020-02-11 15:14:15 +0300

    index updated

--- client/index.js
+++ client/index.js
@@ -1,67 +1,35 @@
 import domready from 'domready'
-import initSubscription from './init_subscription'
-import createApi from './create_api'
+import { getApi } from 'resolve-api'
 import initUI from './init_ui'
-import updateUI from './update_ui'
 
-const main = async ({
-  subscribeAdapter: createSubscribeAdapter,
-  rootPath,
-  viewModels
-}) => {
-  try {
-    await new Promise(resolve => domready(resolve))
-    const chatViewModel = viewModels.find(({ name }) => name === 'chat')
-    const api = createApi(rootPath)
+const main = async resolveContext => {
+  await new Promise(resolve => domready(resolve))
+  const api = getApi(resolveContext)
 
-    const sendMessage = (userName, message) =>
-      api
-        .sendCommand({
-          aggregateName: 'Chat',
-          commandType: 'postMessage',
-          aggregateId: userName,
-          payload: message
-        })
-        .catch(error => {
-          // eslint-disable-next-line no-console
-          console.warn(`Error while sending command: ${error}`)
-        })
-
-    let chatViewModelState = window.__INITIAL_STATE__.chat
-
-    initUI(chatViewModelState, sendMessage)
-
-    const chatViewModelUpdater = event => {
-      const eventType = event != null && event.type != null ? event.type : null
-      const eventHandler = chatViewModel.projection[eventType]
-
-      if (typeof eventHandler === 'function') {
-        chatViewModelState = eventHandler(chatViewModelState, event)
+  const sendMessage = (userName, message) =>
+    api.command(
+      {
+        aggregateName: 'Chat',
+        type: 'postMessage',
+        aggregateId: userName,
+        payload: message
+      },
+      err => {
+        if (err) {
+          console.warn(`Error while sending command: ${err}`)
+        }
       }
-
-      setImmediate(updateUI.bind(null, chatViewModelState))
-    }
-
-    chatViewModelUpdater(null)
-
-    const { attachSubscriber, subscribeToTopics } = await initSubscription(
-      createSubscribeAdapter,
-      rootPath,
-      api
     )
 
-    attachSubscriber(chatViewModelUpdater)
+  let chatViewModelState = window.__INITIAL_STATE__.chat
 
-    await subscribeToTopics([
-      {
-        topicName: 'MESSAGE_POSTED',
-        topicId: '*'
-      }
-    ])
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(`Fatal application error: ${error}`)
+  initUI(chatViewModelState, sendMessage)
+
+  const callback1 = event => {
+    console.log('1: --- topic event', event)
+    return event
   }
+  api.subscribeTo('chat', '*', callback1)
 }
 
 export default main
